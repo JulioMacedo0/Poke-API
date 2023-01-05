5d4da709d0a42eaee6c4e5449b0435d606b34985
Julio Macedo <juliocesar1076@gmail.com>
2023-01-04 22:57:13 -0300

    chore: add default colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,3 +1,11 @@
 export const defaultTheme = {
-  background: "#36454F",
+  background: "#eaeef2",
+  green: "#50ccb0",
+  red: "#f57767",
+  blue: "#83c6e1",
+  yellow: "#eed388",
+  title: "#fffaf9",
+  black: "#3e4a48",
+  grey: "#d1d3d5",
+  white: "#ffffff",
 } as const;
