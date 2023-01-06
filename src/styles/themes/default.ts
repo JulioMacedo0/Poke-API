88c81986a6692758be4eacf3057018da448c2c8c
Julio Macedo <juliocesar1076@gmail.com>
2023-01-06 17:36:17 -0300

    feat: add colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -4,6 +4,8 @@ export const defaultTheme = {
   red: "#f57767",
   blue: "#83c6e1",
   yellow: "#eed388",
+  brown: "#553d2a",
+  skin_pink: "#e5cbce",
   title: "#fffaf9",
   black: "#3e4a48",
   grey: "#d1d3d5",
