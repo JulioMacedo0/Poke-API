7382645dc68b1a984aa71c26ee361f8220f50fef
Julio Macedo <juliocesar1076@gmail.com>
2023-01-06 17:36:57 -0300

    feat: move style

--- src/components/StylesComponents/styles.ts
+++ src/components/StylesComponents/styles.ts
@@ -5,13 +5,3 @@ export const Title = styled.p`
   font-size: 1.2rem;
   font-weight: bold;
 `;
-
-export const TypeSpan = styled.span`
-  color: ${(props) => props.theme["title"]};
-  background-color: ${(props) => props.theme["green"]};
-
-  padding: 0.1rem 1rem;
-  font-size: 0.75rem;
-  filter: brightness(110%);
-  border-radius: 99999px;
-`;
