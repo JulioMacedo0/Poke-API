1c5aeb05e3453d484674ed70c2bd8276a82cfbd7
Julio Macedo <juliocesar1076@gmail.com>
2023-01-06 13:53:11 -0300

    feat: get pokemons from api

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,29 +1,74 @@
+import { useEffect, useState } from "react";
+import { api } from "../../api/api";
 import { Card } from "../Card";
 import * as S from "./styles";
 
+interface ArrayPokemons {
+  name: string;
+  url: string;
+}
+
+interface Pokemons {
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+    };
+  };
+  name: string;
+  id: number;
+}
+
+interface CardsType {
+  next: string | null;
+  previous: string | null;
+  results: ArrayPokemons[];
+}
+
 export const Content = () => {
+  const [Pokemons, setPokemons] = useState<Pokemons[]>([]);
+
+  // const paginate = {
+  //   next: Cards.next,
+  //   previous: Cards.previous,
+  // };
+
+  useEffect(() => {
+    api.get("/pokemon").then((response) => {
+      const requests = response.data.results.map((obj: ArrayPokemons) =>
+        fetch(obj.url)
+      );
+
+      Promise.all(requests)
+        .then((responses) => {
+          // All responses are resolved successfully
+          return Promise.all(responses.map((response) => response.json()));
+        })
+        .then((data: Pokemons[]) => {
+          // This is an array of the response data in JSON format
+          console.log(data + " data in then");
+          setPokemons(data);
+        })
+        .catch((error) => {
+          // If any of the requests fail, this catch block will be executed
+          console.error(error);
+        });
+    });
+  }, []);
+
+  console.log(Pokemons);
   return (
     <S.Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {Pokemons.map((pokemon) => {
+        return (
+          <Card
+            key={pokemon.id}
+            name={pokemon.name}
+            pokemonImg={pokemon.sprites.other.dream_world.front_default}
+          />
+        );
+      })}
     </S.Container>
   );
 };
