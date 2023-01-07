6b75eba2cefc9ca7bf395303aabc0debcfdcbbeb
Julio Macedo <juliocesar1076@gmail.com>
2023-01-07 11:28:04 -0300

    feat: create pokemon context

---
+++ src/context/PokemonContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, ReactNode, useContext } from "react";
+
+interface PokemonContextType {}
+
+interface PokemoncontextProps {
+  children: ReactNode;
+}
+const PokemonContext = createContext({} as PokemonContextType);
+
+export const AuthContextProvider = ({ children }: PokemoncontextProps) => {
+  return (
+    <PokemonContext.Provider value={{}}>{children}</PokemonContext.Provider>
+  );
+};
+
+export const usePokemon = () => useContext(PokemonContext);
