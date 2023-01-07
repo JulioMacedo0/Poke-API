eedbf1be60414b3e7df29b8c3da4bd43649c4fce
Julio Macedo <juliocesar1076@gmail.com>
2023-01-07 20:12:35 -0300

    feat: use context

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,73 +1,20 @@
-import { useEffect, useState } from "react";
-import { api } from "../../api/api";
+import { usePokemon } from "../../context/PokemonContext";
+
 import { Card } from "../Card";
 import * as S from "./styles";
 
-interface ArrayPokemons {
-  name: string;
-  url: string;
-}
-
-interface Types {
-  type: {
-    name: string;
-  };
-}
-
-interface Pokemons {
-  sprites: {
-    other: {
-      dream_world: {
-        front_default: string;
-      };
-    };
-  };
-  name: string;
-  id: number;
-  types: Types[];
-}
-
-interface CardsType {
-  next: string | null;
-  previous: string | null;
-  results: ArrayPokemons[];
-}
+// interface CardsType {
+//   next: string | null;
+//   previous: string | null;
+//   results: ArrayPokemons[];
+// }
 
 export const Content = () => {
-  const [Pokemons, setPokemons] = useState<Pokemons[]>([]);
-
-  // const paginate = {
-  //   next: Cards.next,
-  //   previous: Cards.previous,
-  // };
-
-  useEffect(() => {
-    api.get("/pokemon").then((response) => {
-      const requests = response.data.results.map((obj: ArrayPokemons) =>
-        fetch(obj.url)
-      );
-
-      Promise.all(requests)
-        .then((responses) => {
-          // All responses are resolved successfully
-          return Promise.all(responses.map((response) => response.json()));
-        })
-        .then((data: Pokemons[]) => {
-          // This is an array of the response data in JSON format
-          console.log(data + " data in then");
-          setPokemons(data);
-        })
-        .catch((error) => {
-          // If any of the requests fail, this catch block will be executed
-          console.error(error);
-        });
-    });
-  }, []);
+  const { pokemons, page, GetPokemons } = usePokemon();
 
   return (
     <S.Container>
-      {Pokemons.map((pokemon) => {
-        console.log(pokemon.types + " type test");
+      {pokemons?.map((pokemon) => {
         return (
           <Card
             key={pokemon.id}
@@ -77,6 +24,10 @@ export const Content = () => {
           />
         );
       })}
+      <button disabled={page - 1 < 1} onClick={() => GetPokemons(page - 1)}>
+        Previous Page
+      </button>
+      <button onClick={() => GetPokemons(page + 1)}>Next Page</button>
     </S.Container>
   );
 };
