5fae270650bfa8ed1b394ad87adedc237bc686c8
Julio Macedo <juliocesar1076@gmail.com>
2023-01-07 21:34:57 -0300

    feat: create overlay

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -20,4 +20,44 @@ export const ButtonsContainer = styled.div`
   gap: 1rem;
   flex-wrap: wrap;
   justify-content: center;
+
+  button {
+    background-color: #4caf50; /* Green */
+    border: none;
+    color: white;
+    padding: 8px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+
+  button:disabled {
+    background-color: #ccc;
+    color: #666;
+    cursor: default;
+  }
+  button:not(:disabled):hover {
+    background-color: #3e8e41;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Overlay = styled.div`
+  position: fixed; /* or absolute */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7); /* semi-transparent black */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
