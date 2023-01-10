9d44d59f3ad0b76f38f8ba65df10cdf280993482
Julio Macedo <juliocesar1076@gmail.com>
2023-01-09 23:17:21 -0300

    feat: add new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Pokemon } from "./pages/Pokemon";
 
-export const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
+export const Router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<Home />} />,
+    <Route path="/pokemon" element={<Pokemon />} />,
+  ])
+);
