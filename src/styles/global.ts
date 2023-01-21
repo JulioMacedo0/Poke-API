0f92ed33144fc3b7e22e534f4945988e580b076f
Julio Macedo <juliocesar1076@gmail.com>
2023-01-21 08:31:24 -0300

    feat: estyle scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,10 +13,33 @@ body {
   height: 100%;
    background-color: ${(props) => props.theme["background"]};
     -webkit-font-smoothing: antialiased;
+
+    /* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
 }
 
 #root {
   min-height: 100%;
+
+
 }
 button {
   cursor: pointer;
