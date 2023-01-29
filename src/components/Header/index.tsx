7a15a8d409f4008d4bc15f46dddec65fcdce8532
Julio Macedo <juliocesar1076@gmail.com>
2023-01-29 08:53:55 -0300

    feat: add onChange and getPokemon function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import { MagnifyingGlass } from "phosphor-react";
 import Logo from "../../assets/pokeball.svg";
+import { usePokemon } from "../../context/PokemonContext";
 import * as S from "./styles";
 
 export const Header = () => {
+
+  const { getPokemon, onChangeInput, input } = usePokemon();
   return (
     <S.HeaderContainer>
       <S.HeaderWrap>
         <S.ImgLogo src={Logo} />
         <div>
           <MagnifyingGlass size={22} />
-          <input type="text" />
-          <button>Search</button>
+          <input type="text" onChange={ (event) => onChangeInput(event.target.value)}/>
+          <button onClick={ () => getPokemon(input)}>Search</button>
         </div>
       </S.HeaderWrap>
     </S.HeaderContainer>
