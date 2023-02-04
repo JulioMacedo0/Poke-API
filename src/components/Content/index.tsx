43ad4a2348df675af9f4a633d72f4c662b618d2f
Julio Macedo <juliocesar1076@gmail.com>
2023-02-04 18:55:54 -0300

    refactoring: ident code

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,9 +5,9 @@ import * as S from "./styles";
 import Lottie from "react-lottie";
 import pokeball from "../../lotties/pokeball.json";
 import { useRef } from "react";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 const defaultOptions = {
   loop: true,
   autoplay: true,
@@ -17,106 +17,112 @@ const defaultOptions = {
   },
 };
 
-
-
 export const Content = () => {
-  const { pokemons, pokemon, page, isLoading, getPokemons, clearSearchPokemon } = usePokemon();
+  const {
+    pokemons,
+    pokemon,
+    page,
+    isLoading,
+    getPokemons,
+    clearSearchPokemon,
+  } = usePokemon();
   const Ref = useRef<HTMLDivElement | null>(null);
   const navigate = useNavigate();
 
-
-
   return (
     <>
-    <ToastContainer
-position="top-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-/>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <S.Container ref={Ref}>
-        {
-          pokemon.name && <Card
-          onClick={() => navigate(`/pokemon`, {state: {
-            name: pokemon.name,
-            pokemonImg: pokemon.sprites?.other.dream_world.front_default,
-            pokemonTypes: pokemon.types,
-            stats: pokemon.stats,
-            height: pokemon.height,
-            weight: pokemon.weight,
-            abilities: pokemon.abilities,
-            base_experience: pokemon.base_experience,
-            moves: pokemon.moves,
-            species: pokemon.species
-          }}
-          )}
+        {pokemon.name && (
+          <Card
+            onClick={() =>
+              navigate(`/pokemon`, {
+                state: {
+                  name: pokemon.name,
+                  pokemonImg: pokemon.sprites?.other.dream_world.front_default,
+                  pokemonTypes: pokemon.types,
+                  stats: pokemon.stats,
+                  height: pokemon.height,
+                  weight: pokemon.weight,
+                  abilities: pokemon.abilities,
+                  base_experience: pokemon.base_experience,
+                  moves: pokemon.moves,
+                  species: pokemon.species,
+                },
+              })
+            }
             name={pokemon.name}
             pokemonImg={pokemon.sprites?.other.dream_world.front_default}
             types={pokemon.types}
           />
-        }
-        {!pokemon.name && pokemons?.map((pokemon) => {
-          return (
-            <Card
-              onClick={() => navigate(`/pokemon`, {state: {
-                name: pokemon.name,
-                pokemonImg: pokemon.sprites.other.dream_world.front_default,
-                pokemonTypes: pokemon.types,
-                stats: pokemon.stats,
-                height: pokemon.height,
-                weight: pokemon.weight,
-                abilities: pokemon.abilities,
-                base_experience: pokemon.base_experience,
-                moves: pokemon.moves,
-                species: pokemon.species
-              }}
-              )}
-              key={pokemon.id}
-              name={pokemon.name}
-              pokemonImg={pokemon.sprites.other.dream_world.front_default}
-              types={pokemon.types}
-            />
-          );
-        })
-        }
+        )}
+        {!pokemon.name &&
+          pokemons?.map((pokemon) => {
+            return (
+              <Card
+                onClick={() =>
+                  navigate(`/pokemon`, {
+                    state: {
+                      name: pokemon.name,
+                      pokemonImg:
+                        pokemon.sprites.other.dream_world.front_default,
+                      pokemonTypes: pokemon.types,
+                      stats: pokemon.stats,
+                      height: pokemon.height,
+                      weight: pokemon.weight,
+                      abilities: pokemon.abilities,
+                      base_experience: pokemon.base_experience,
+                      moves: pokemon.moves,
+                      species: pokemon.species,
+                    },
+                  })
+                }
+                key={pokemon.id}
+                name={pokemon.name}
+                pokemonImg={pokemon.sprites.other.dream_world.front_default}
+                types={pokemon.types}
+              />
+            );
+          })}
       </S.Container>
       <S.ButtonsContainer>
+        {!pokemon.name && pokemons.length && (
+          <>
+            <button
+              disabled={page - 1 < 1}
+              onClick={() => {
+                getPokemons(page - 1);
+                Ref.current?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Previous Page
+            </button>
+            <span>{page}</span>
+            <button
+              onClick={() => {
+                getPokemons(page + 1);
+                Ref.current?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Next Page
+            </button>
+          </>
+        )}
 
-          {
-            !pokemon.name && pokemons.length &&
-        (
-        <>
-        <button
-          disabled={page - 1 < 1}
-          onClick={() => {
-            getPokemons(page - 1);
-            Ref.current?.scrollIntoView({ behavior: "smooth" });
-          }}
-        >
-          Previous Page
-        </button>
-        <span>{page}</span>
-        <button
-          onClick={() => {
-            getPokemons(page + 1);
-            Ref.current?.scrollIntoView({ behavior: "smooth" });
-          }}
-        >
-          Next Page
-        </button>
-        </>
-        )
-   }
-
-   {
-    pokemon.name && (<button onClick={() => clearSearchPokemon()}>Go home</button>)
-   }
+        {pokemon.name && (
+          <button onClick={() => clearSearchPokemon()}>Go home</button>
+        )}
         {isLoading && (
           <S.Overlay>
             <Lottie options={defaultOptions} height={150} width={150} />
