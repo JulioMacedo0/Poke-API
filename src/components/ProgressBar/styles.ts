f73438b973d38ffc0f0f6bcf91ac4a7d689cd754
Julio Macedo <juliocesar1076@gmail.com>
2023-02-04 18:56:34 -0300

    refactoring: ident code

--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -8,15 +8,16 @@ interface ProgressBarProps {
 export const ProgressBar = styled.div`
   width: 100%;
   height: 0.45rem;
-  background: ${props => props.theme.light_grey};
+  background: ${(props) => props.theme.light_grey};
   position: relative;
   border-radius: 10px;
 
   &::before {
     content: "";
-    width: ${({ currentValue, maxValue }: ProgressBarProps) => `calc(${currentValue} / ${maxValue} * 100%)`};
+    width: ${({ currentValue, maxValue }: ProgressBarProps) =>
+      `calc(${currentValue} / ${maxValue} * 100%)`};
     height: 100%;
-    background: ${props => props.theme.green};
+    background: ${(props) => props.theme.green};
     border-radius: 10px;
     position: absolute;
   }
