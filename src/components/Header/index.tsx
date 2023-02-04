efc6ce9928a1c5cae92c9c056dfc7fc606506b6c
Julio Macedo <juliocesar1076@gmail.com>
2023-02-04 18:56:11 -0300

    refactoring: ident code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,20 +4,27 @@ import { usePokemon } from "../../context/PokemonContext";
 import * as S from "./styles";
 
 export const Header = () => {
-
   const { getPokemon, onChangeInput, input } = usePokemon();
   return (
     <S.HeaderContainer>
       <S.HeaderWrap>
         <S.ImgLogo src={Logo} />
         <div>
-          <MagnifyingGlass size={22} />
-          <input value={input} type="text" onChange={ (event) => onChangeInput(event.target.value)} onKeyDown={(event) => {
-            if (event.keyCode === 13 || event.keyCode === 10 || event.key === 'Tab' ) {
-              getPokemon(input);
+          <input
+            value={input}
+            type="text"
+            onChange={(event) => onChangeInput(event.target.value)}
+            onKeyDown={(event) => {
+              if (
+                event.keyCode === 13 ||
+                event.keyCode === 10 ||
+                event.key === "Tab"
+              ) {
+                getPokemon(input);
               }
-          }}/>
-          <button onClick={ () => getPokemon(input)}>Search</button>
+            }}
+          />
+          <button onClick={() => getPokemon(input)}>Search</button>
         </div>
       </S.HeaderWrap>
     </S.HeaderContainer>
