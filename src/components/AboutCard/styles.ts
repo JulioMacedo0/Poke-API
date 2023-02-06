48bc4bae31e6553764b2c7aad7c149d3361cdc7e
Julio Macedo <juliocesar1076@gmail.com>
2023-02-06 13:05:47 -0300

    feat: create loading

--- src/components/AboutCard/styles.ts
+++ src/components/AboutCard/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { GetColor } from "../../helpers/get-color";
+
+interface loadingProps {
+  type: string;
+}
 
 export const Container = styled.div`
-margin: 1rem;
+  margin: 1rem;
   padding: 1rem;
   display: flex;
   align-items: center;
@@ -12,10 +17,27 @@ margin: 1rem;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 1px 5px;
 `;
 
-
 export const Paragraph = styled.p`
+  color: ${(props) => props.theme.dark_black};
+  font-size: 0.95rem;
+`;
 
-color: ${(props) =>  props.theme.dark_black};
-font-size: 0.95rem;
+export const loading = styled.div<loadingProps>`
+  width: 2rem;
+  height: 2rem;
+  border: 3px solid ${(props) => GetColor(props.type)};
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
 
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
